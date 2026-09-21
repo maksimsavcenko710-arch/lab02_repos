1447f203dd2426dcf31d24c6b3871501c0c5af24
Your Name <you@example.com>
2026-09-22 01:24:50 +0300

    замінив на р1 р2

--- pr2/pr2.cpp
+++ pr2/pr2.cpp
@@ -12,17 +12,17 @@ using namespace std;
 int main()
 {
     double alpha; // вхідний параметр
-    double o1;    // результат обчислення 1-го виразу
-    double o2;    // результат обчислення 2-го виразу
+    double p1;    // результат обчислення 1-го виразу
+    double p2;    // результат обчислення 2-го виразу
 
     cout << "alpha = "; cin >> alpha;
 
-    o1 = (sin(alpha) + sin(5 * alpha) - sin(3 * alpha)) / (cos(alpha) - cos(3 * alpha) + cos(5 * alpha));
-    o2 = tan(3 * alpha);
+    p1 = (sin(alpha) + sin(5 * alpha) - sin(3 * alpha)) / (cos(alpha) - cos(3 * alpha) + cos(5 * alpha));
+    p2 = tan(3 * alpha);
 
     cout << endl;
-    cout << "o1 = " << o1 << endl;
-    cout << "o2 = " << o2 << endl;
+    cout << "p1 = " << p1 << endl;
+    cout << "p2 = " << p2 << endl;
 
     cin.get();
     return 0;
