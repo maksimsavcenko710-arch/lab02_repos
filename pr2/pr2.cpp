ec2a9cb478267c0b05fe5da75f43f6338491f842
Your Name <you@example.com>
2026-09-22 00:48:23 +0300

    замінив а на альфа

--- pr2/pr2.cpp
+++ pr2/pr2.cpp
@@ -11,14 +11,14 @@ using namespace std;
 
 int main()
 {
-    double a; // вхідний параметр
+    double alpha; // вхідний параметр
     double z1;    // результат обчислення 1-го виразу
     double z2;    // результат обчислення 2-го виразу
 
-    cout << "a = "; cin >> a;
+    cout << "alpha = "; cin >> alpha;
 
-    z1 = (sin(a) + sin(5 * a) - sin(3 * a)) / (cos(a) - cos(3 * a) + cos(5 * a));
-    z2 = tan(3 * a);
+    z1 = (sin(alpha) + sin(5 * alpha) - sin(3 * alpha)) / (cos(alpha) - cos(3 * alpha) + cos(5 * alpha));
+    z2 = tan(3 * alpha);
 
     cout << endl;
     cout << "z1 = " << z1 << endl;
