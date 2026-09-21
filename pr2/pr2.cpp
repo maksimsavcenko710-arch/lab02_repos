f818aeab5902a45c3f0c88ab0e19b2059d78457d
Your Name <you@example.com>
2026-09-22 00:41:19 +0300

    виконав завдання 2.1

--- pr2/pr2.cpp
+++ pr2/pr2.cpp
@@ -1,17 +1,29 @@
+// Lab_02.cpp
+// Савченко Максим В'ячеславович
+// Лабораторна робота № 2.
+// Лінійні програми.
+// Варіант 24
+
 #include <iostream>
+#include <cmath>
+
 using namespace std;
+
 int main()
 {
-	double x; // вхідний параметр
-	double z1; // результат обчислення 1-го виразу
-	// double z2; // результат обчислення 2-го виразу
-	cout << "x = "; cin >> x;
-	//z1 = 2 * x;
-		z2 = x + x;
-	cout << endl;
-	// cout << "z1 = " << z1 << endl;
-		cout << "z2 = " << z2 << endl;
-	cin.get();
-	return 0;
+    double a; // вхідний параметр
+    double z1;    // результат обчислення 1-го виразу
+    double z2;    // результат обчислення 2-го виразу
+
+    cout << "a = "; cin >> a;
+
+    z1 = (sin(a) + sin(5 * a) - sin(3 * a)) / (cos(a) - cos(3 * a) + cos(5 * a));
+    z2 = tan(3 * a);
+
+    cout << endl;
+    cout << "z1 = " << z1 << endl;
+    cout << "z2 = " << z2 << endl;
+
+    cin.get();
+    return 0;
 }
-			
